0cc7e8e6b0efc8324d6b53cf3f4e1d4f31773845
Hamad Naveed <56270097+HNaveed13@users.noreply.github.com>
2023-02-17 01:33:57 -0500

    Update mymalloc.c

--- Assignment 1 (my little malloc)/mymalloc.c
+++ Assignment 1 (my little malloc)/mymalloc.c
@@ -9,19 +9,83 @@ typedef struct node {
     struct node *link;
 } node;
 
-void mymalloc(size_t size, char *file, int line) {
+static node *head = NULL;
+
+void* mymalloc(size_t size, char *file, int line) {
+
     if(size == 0){
-        printf("ERROR: Insufficient memory!");
-        return;
-    }else if(MEMSIZE - size < 0){
-        printf("ERROR: Not enough space!");
-        return;
+        printf("ERROR: insufficient memory!");
+        return NULL;
     }
+
+
+    head = (node*)memory;
+    head->size = MEMSIZE - sizeof(node);
+    head->link = NULL;
+
+    // Round up the requested size to a multiple of the word size (to ensure proper alignment)
+    size = (size + sizeof(void*) - 1) / sizeof(void*) * sizeof(void*);
+
+    // Search the free list for a block of the requested size
+    node *prev = NULL;
+    node *current = head;
+    while (current != NULL) {
+        if (current->size >= size) {
+            // We found a block of the requested size
+            if (current->size - size >= sizeof(node)) {
+                // Split the block into an allocated block and a free block
+                node *new_node = (node*)((char*)current + sizeof(node) + size);
+                new_node->size = current->size - size - sizeof(node);
+                new_node->link = current->link;
+                current->size = size;
+                current->link = new_node;
+            }
+            if (prev == NULL) {
+                head = current->link;
+            } else {
+                prev->link = current->link;
+            }
+            return (void*)(current + 1);
+        }
+        prev = current;
+        current = current->link;
+    }
+
+    printf("ERROR: Size requested not found!");
+    return NULL;
 }
 
-void myfree(void *ptr, char *file, int line){
-    if(ptr == NULL){
-        printf("ERROR: Pointer empty!");
+void myfree(void *ptr, char *file, int line) {
+    if (ptr == NULL) {
+        printf("Empty pointer!");
         return;
+    }
+
+    // Get the node that corresponds to the allocated block
+    node *current = (node*)ptr - 1;
+
+    // Add the block to the free list
+    node *prev = NULL;
+    node *free_node = (node*)memory;
+    while (free_node != NULL && free_node < current) {
+        prev = free_node;
+        free_node = free_node->link;
+    }
+    if (prev == NULL) {
+        head = current;
+    } else {
+        prev->link = current;
+    }
+    current->link = free_node;
+
+    // Coalesce adjacent free blocks
+    if (current->link != NULL && (char*)current + current->size + sizeof(node) == (char*)current->link) {
+        current->size += current->link->size + sizeof(node);
+        current->link = current->link->link;
+    }
+    if (prev != NULL && (char*)prev + prev->size + sizeof(node) == (char*)current) {
+        prev->size += current->size + sizeof(node);
+        prev->link = current->link;
+        current = prev;
     }
 }
